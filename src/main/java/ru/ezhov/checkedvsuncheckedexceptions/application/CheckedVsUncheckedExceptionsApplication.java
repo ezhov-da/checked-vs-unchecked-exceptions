c49c45c8cfc8ad389f82e51ca7b9bb6fb13eabba
ezhov-da <ezhdenis@yandex.ru>
2022-09-20 13:20:01 +0300

    feat: init project

---
+++ src/main/java/ru/ezhov/checkedvsuncheckedexceptions/application/CheckedVsUncheckedExceptionsApplication.java
@@ -0,0 +1,13 @@
+package ru.ezhov.checkedvsuncheckedexceptions.application;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckedVsUncheckedExceptionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckedVsUncheckedExceptionsApplication.class, args);
+	}
+
+}
